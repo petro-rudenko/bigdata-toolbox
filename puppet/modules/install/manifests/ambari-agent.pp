269ab7192914a3168a10091f7c947c7d540c43e7
Peter Rudenko <petro.rudenko@gmail.com>
2014-05-28 20:36:26 +0300

    Fix ambari repo

--- puppet/modules/install/manifests/ambari-agent.pp
+++ puppet/modules/install/manifests/ambari-agent.pp
@@ -1,7 +1,7 @@
 class install::ambari-agent {
 
   exec{"ambari-repo":
-    command => "wget -O /etc/yum.repos.d/ambari.repo http://dev.hortonworks.com.s3.amazonaws.com/ambari/centos6/1.x/updates/1.6.1.trunk/ambari.repo",
+    command => "wget -O /etc/yum.repos.d/ambari.repo http://s3.amazonaws.com/dev.hortonworks.com/ambari/centos6/1.x/updates/1.6.1/ambari.repo",
     require => Package["wget"],
     unless => "ls -al /etc/yum.repos.d/ambari.repo"
   }
